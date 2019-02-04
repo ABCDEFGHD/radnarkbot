ced78d3d5209e64df91b372c4d201a4ec2732cbf
ABCDEFGH <38960954+ABCDEFGHD@users.noreply.github.com>
2019-02-04 18:25:28 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+
+const weathers = ["Il va faire beau aujourd'hui", "Il va pleuvoir aujourd'hui", "Il va neiger aujourd'hui", "Ça va être nuageux aujourd'hui"];
+const prefix = ("/");
+const epref = "**❌ | ";
+
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: `Garder le Monde`, type: 0}});
+    bot.user.setStatus("dnd");
+    console.log("Bot Prêt !");
+});
+
+bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+    if (message.content.startsWith(prefix + "help")) {
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#E81414')
+            .addField("Prefix", `${prefix}`)
+            .addField("Commandes", "- help: Montre ce menu\n- roll: donne un chiffre aléatoire entre 0 (echec) et 100 (réussite)")
+            .setFooter(message.guild.name, `${message.guild.iconURL}`)
+            .setTimestamp();
+        message.channel.send(help_embed);
+    }
+
+    if (message.content.startsWith(prefix + "roll")){
+        var randum = getRandomInt(0, 100);
+        if (randum < 5) {
+            message.channel.sendMessage(`Echec Critique **${randum2}**`);
+            console.log(randum2);
+        } else if (randum > 95) {
+            message.channel.sendMessage(`Réussite Critique **${randum2}**`);
+            console.log(randum2);
+        } else {
+            message.channel.sendMessage(`Roll: **${randum2}**`);
+            console.log(randum2);
+        }
+        bot.channels.findAll('name', 'logs-pandabot').map(channel => channel.send("Commande : __roll__ par : **" + message.author.tag + "** Dans **" + message.guild.name + "** / **" + message.channel.name + "** avec comme nombre **" + randum + "**"));
+    }
+});
+
+bot.setInterval(randomWheather, 30000);
